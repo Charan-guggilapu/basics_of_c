f3eb510654de9493d4c2a3196835000627c1c86e
Charan-guggilapu <147476551+Charan-guggilapu@users.noreply.github.com>
2023-10-11 12:07:38 +0530

    Add files via upload

---
+++ LIMITS.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+	printf("short:%hd to %hd\n",SHRT_MIN,SHRT_MAX);
+	printf("long:%hl to %hl\n",LONG_MIN,LONG_MAX);
+	printf("long long:%hll to %hll\n",LLONG_MIN,LLONG_MAX);
+	printf("long:%hl to %hl\n",LONG_MIN,LONG_MAX);
+	
+	
+	
+}
